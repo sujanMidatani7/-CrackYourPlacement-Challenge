f364610da5ac717870e06909e511a931b475c59d
Sujan midatani <sujanmidatani7@gmail.com>
2024-07-21 23:53:33 +0530

    Create 287FindtheDuplicateNumber.java

---
+++ Arrays/287FindtheDuplicateNumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findDuplicate(int[] arr) {
+        int n = arr.length;
+        int freq[] = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            if (freq[arr[i]] == 0) {
+                freq[arr[i]] += 1;
+            } else {
+                return arr[i];
+            }
+        }
+        return 0;
+    }
+}
