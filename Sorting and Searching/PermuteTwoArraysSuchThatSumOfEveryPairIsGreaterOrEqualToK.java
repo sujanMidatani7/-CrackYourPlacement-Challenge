ae78300f06d5201c38654000d1924b8a81dcab46
Sujan midatani <sujanmidatani7@gmail.com>
2024-07-21 19:14:22 +0530

    Create PermuteTwoArraysSuchThatSumOfEveryPairIsGreaterOrEqualToK.java

---
+++ Sorting and Searching/PermuteTwoArraysSuchThatSumOfEveryPairIsGreaterOrEqualToK.java
@@ -0,0 +1,16 @@
+boolean isPossible(Integer a[], int b[],
+                                  int n, int k) 
+{
+    // Sort the array a[] in decreasing order.
+    Arrays.sort(a, Collections.reverseOrder());
+
+    // Sort the array b[] in increasing order.
+    Arrays.sort(b);
+
+    // Checking condition on each index.
+    for (int i = 0; i < n; i++)
+    if (a[i] + b[i] < k)
+        return false;
+
+    return true;
+}
